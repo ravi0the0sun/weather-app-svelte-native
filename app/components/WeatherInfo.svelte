f86354196eecacab57b842aa226feb481fd2e169
ravi0the0sun <punravi73@gmail.com>
2020-07-17 19:56:05 +1000

    added loading component

--- app/components/WeatherInfo.svelte
+++ app/components/WeatherInfo.svelte
@@ -11,32 +11,36 @@
     const iconUrl = `https://openweathermap.org/img/wn/${icon}@4x.png`;
 
 </script>
-
-
-<image class="img-center" src="{iconUrl}" />
-<label class="weather-temp" text="{temp}°" />
-<label class="weather-des" text="{description}"/>
-<label class="weather-main" text={main}/>
+<gridLayout horizontalAlignment="center" verticalAlignment="center" textWrap="true">
+    <image col="0" class="img-center" src="{iconUrl}" />
+    <label col="1" class="weather-temp" text="{temp}°" />
+    <label col="2" class="weather-des" text="{description}"/>
+    <label col="3" class="weather-main" text={main}/>
+</gridLayout>
 
 
 <style>
     .img-center {
+        background-color: blue;
         position: absolute;
         width: 200px;
         height: 200px;
         top: 10%;
     }
     .weather-temp {
+        background-color: red;
         position:absolute;
         font-size: 40px;
         color: #ffffff;
     }
     .weather-des {
+        background-color: pink;
         top:100px;
         color:#ffffff;
         text-transform: capitalize; 
     }
     .weather-main {
+        background-color: yellow;
         font-size: 20;
         color: #ffffff;
         font-weight: 500px;
