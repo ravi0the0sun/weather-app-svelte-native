ad4adb3c9d4d055989006982c596d8060ff2ed95
ravi0the0sun <punravi73@gmail.com>
2020-07-19 19:43:00 +1000

    added to activityIndicator in WeatherIcon.svelte

--- app/components/WeatherIcon.svelte
+++ app/components/WeatherIcon.svelte
@@ -9,20 +9,20 @@
     const {
         weather: [details],
     } = $currentweather;
-    const { id, icon } = details;
+    let { id, icon } = details;
 
 </script>
 
-{#if (id < 300)}
+{#if (id > 199 && id < 300)}
     <label col="{col}" row="{row}" class="fas image" text="&#xf0e7;"/>
 {:else if (id < 400)}
     <label col="{col}" row="{row}" class="fas image" text="&#xf73d;"/>
-{:else if (id < 505)}
+{:else if (id > 501 && id < 505)}
     <label col="{col}" row="{row}" class="fas image" text="&#xf743;"/>
+{:else if (id > 505 && id < 532)}
+    <label col="{col}" row="{row}" class="fas image" text="&#xf740;"/>
 {:else if (id === 500 || id < 700)}
     <label col="{col}" row="{row}" class="fas image" text="&#xf2dc;"/>
-{:else if (id < 532)}
-    <label col="{col}" row="{row}" class="fas image" text="&#xf740;"/>
 {:else if (id < 800)}
     <label col="{col}" row="{row}" class="fas image" text="&#xf75f;"/>
 {:else if (id === 800)}
@@ -40,12 +40,11 @@
         <label col="{col}" row="{row}" class="fas image" text="&#xf0c2;"/>
     {/if}
 {:else}
-    <label col="{col}" row="{row}" class="fas image" text="&#xf743;"/>
+    <activityIndicator col="{col}" row="{row}" busy="{true}" color="#ffffff" width="50" height="50"></activityIndicator>
 {/if}
-
 <style>
     .image {
-        margin-top: 20px;
+        margin-top: 130px;
         font-size: 80px;
     }
 </style>
